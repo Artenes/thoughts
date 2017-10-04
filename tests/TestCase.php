2b6d529e67b50cd903c35932e1bf60846f1fdeab
Artenes Nogueira <artenesama@gmail.com>
2017-10-04 08:13:49 -0300

    Disable exception handling for all tests by default

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,25 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * Base test case class.
+ *
+ * @package Tests
+ */
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Set up some things for all tests.
+     */
+    protected function setUp()
+    {
+
+        parent::setUp();
+
+        $this->withoutExceptionHandling();
+
+    }
+
 }

f7669cec0ee0d10b21e42e057e77cdf44372390f
Artenes Nogueira <artenesama@gmail.com>
2017-10-06 21:46:21 -0300

    Add test users to database seed

--- database/seeds/ThoughtsTableSeeder.php
+++ database/seeds/ThoughtsTableSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Thoughts\Follower;
 use Thoughts\Like;
 use Thoughts\Searchable;
 use Thoughts\Thought;
+use Thoughts\User;
 
 /**
  * Seeds thoughts in database.
@@ -19,15 +21,16 @@ class ThoughtsTableSeeder extends Seeder
     public function run()
     {
 
-        $index = new Searchable();
+        //Known test facebook users
+        $userA = factory(User::class)->create(['email' => 'open_lqwmgpj_user@tfbnw.net', 'facebook_id' => '111308292957826']);
+        $userB = factory(User::class)->create(['email' => 'artenesama@gmail.com', 'facebook_id' => '1392574914180319']);
 
-        factory(Thought::class, 50)->create()->each(function ($thought) use ($index) {
+        factory(Thought::class, 50)->create()->each(function ($thought) use ($userA, $userB) {
 
             factory(Like::class, random_int(1, 100))->create(['thought_id' => $thought->id]);
 
-            $index->indexResource($thought);
-
-            $index->indexResource($thought->user);
+            Follower::create($userA, $thought->user);
+            Follower::create($userB, $thought->user);
 
         });
 
